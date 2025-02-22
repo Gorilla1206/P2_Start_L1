87dd6ddaca374207f3b2072c5e3c7f257311477d
Gorilla1206 <rosius.daan@gmail.com>
2025-02-22 10:48:53 +0100

    les 1 python 2

--- startcode/dobbelspel/main.py
+++ startcode/dobbelspel/main.py
@@ -1,1 +1,21 @@
-# Plaats hierin de main-functie
+from operator import truediv
+import random
+
+def is_getalgeraden(gok, geheim_nummer ):
+   if gok == geheim_nummer:
+       print("correct")
+       return True
+   else:
+       print("fout")
+       return False
+
+def raad_het_getal(bovengrens):
+    geheim_nummer = random.randit(1, bovengrens)
+    einde_spel = False
+    gok =int(input("geef een getal"))
+    geraden = is_getal_geraden (gok, geheim_nummer)
+    if geraden == True:
+        einde_spel = True
+        raad_het_getal(10)
+
+
--- startcode/tictactoe/main.py
+++ startcode/tictactoe/main.py
@@ -1,2 +1,30 @@
 # Maak een functie tic_tac_toe(), die ons hele hoofdprogramma bevat
+from startcode.tictactoe.mytictactoe import initialiseer_bord, print_bord, controleer_horizontaal, controleer_winnaar, \
+    zet
 
+
+def tic_tac_toe():
+    bord = initialiseer_bord()
+    huidige_speler = 'X'
+    winnaar = " "
+    teller = 0
+    einde_spel = False
+    while not einde_spel:
+        print_bord(bord)
+
+        rij = int(input("Doe een zet "))
+        kolom = int(input("DOE een zet in een kolom"))
+
+        bord = zet(bord, rij, kolom, huidige_speler)
+
+        if controleer_winnaar(bord) == True:
+            print(f"{huidige_speler} heeft gewonnen")
+            return
+
+
+        if huidige_speler == "X":
+            huidige_speler = "0"
+        else:huidige_speler = "X"
+
+
+tic_tac_toe()
